fb3a7166668052bab7d7813d15d18b1a4e97ab74
tarabario <mtarabario@gmail.com>
2023-02-13 22:56:54 +0200

    remade hero-support with slider; script the slider; conditions was defined for changing window.innerWidth

--- js/main.js
+++ js/main.js
@@ -1,10 +1,42 @@
 const hamburger = document.querySelector('.hamburger');
 const headerNav = document.querySelector('nav[role="primary"]');
-const body = document.body;
-console.log(headerNav);
 
-hamburger.addEventListener('click', () => {
-	hamburger.classList.toggle('to-cross-mark');
-	document.body.classList.toggle('blur');
-	headerNav.classList.toggle('show');
+const buttonPrev = document.querySelector('.button-previous');
+const buttonNext = document.querySelector('.button-next');
+const supportCards = document.querySelectorAll('.support-card');
+const cardsSlider = document.querySelector('.cards-slider');
+
+window.addEventListener('resize', function() {
+	if (this.innerWidth <= 450) {
+		hamburger.addEventListener('click', () => {
+			hamburger.classList.toggle('to-cross-mark');
+			document.body.classList.toggle('blur');
+			headerNav.classList.toggle('show');
+		})
+
+		let activeCardIndex = 0;
+
+		buttonNext.addEventListener('click', () => {
+			if (activeCardIndex === (supportCards.length - 1)) {
+				activeCardIndex = 0;
+				cardsSlider.style.transform = `translateX(0)`;
+			} else {
+				activeCardIndex++;
+				cardsSlider.style.transform = `translateX(-${activeCardIndex * (100 / supportCards.length)}%)`;
+			}
+		});
+
+		buttonPrev.addEventListener('click', () => {
+			if (activeCardIndex === 0) {
+				activeCardIndex = (supportCards.length - 1);
+				cardsSlider.style.transform = `translateX(-${activeCardIndex * (100 / supportCards.length)}%)`;
+			} else {
+				activeCardIndex--;
+				cardsSlider.style.transform = `translateX(-${activeCardIndex * (100 / supportCards.length)}%)`;
+			}
+		});
+	} else if (this.innerWidth > 450) {
+		cardsSlider.style.transform = `translateX(0)`;
+	}
 })
+
